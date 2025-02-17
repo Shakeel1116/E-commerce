aac94348ee201b6bf619b2b0b41c4ee47567573a
Shakeel Shaik <shaikshakeel860@gmail.com>
2025-02-17 07:53:33 +0530

    Deploying Vite to Render

--- FRONTEND/src/Pages/Login.jsx
+++ FRONTEND/src/Pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://e-commerce-piv1.onrender.com/", {
+      const response = await fetch("http://localhost:9000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- FRONTEND/src/Pages/Signup.jsx
+++ FRONTEND/src/Pages/Signup.jsx
@@ -27,7 +27,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("https://e-commerce-piv1.onrender.com/", {
+      const response = await fetch("http://localhost:9000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
