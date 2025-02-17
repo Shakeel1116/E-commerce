c7a0b6dd96bcb0a88ea077bdcaf41bfa0a27b140
Shakeel Shaik <shaikshakeel860@gmail.com>
2025-02-17 23:40:59 +0530

    noraml file add login  ismup

--- FRONTEND/src/Pages/Login.jsx
+++ FRONTEND/src/Pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://e-commerce-3l3n.onrender.com/api/auth/login", {
+      const response = await fetch("http://localhost:9000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
