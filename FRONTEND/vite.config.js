83dec3c0bb3b3cc58e2fccd7d68e044303271adb
Shakeel Shaik <shaikshakeel860@gmail.com>
2025-02-18 10:44:20 +0530

    cnfige update

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // Correct for Render's static site setup
+  base: "./", // Ensures assets load properly on Render
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Explicitly set output directory
-  }
+  
 });
